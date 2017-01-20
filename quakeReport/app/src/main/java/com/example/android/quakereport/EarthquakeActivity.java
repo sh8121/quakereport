4e3e5d243b0a3eaca507cb256d0c9c966bd079fe
sboo <sboo@ebaykorea.com>
2017-01-20 15:29:38 +0900

    apply orderby preference

--- quakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ quakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -108,12 +108,15 @@ public class EarthquakeActivity extends AppCompatActivity implements android.app
         Log.e(LOG_TAG, "onCreateLoader");
         SharedPreferences prfs = PreferenceManager.getDefaultSharedPreferences(this);
         String magnitude = prfs.getString(getString(R.string.settings_min_magnitude_key), getString(R.string.settings_min_magnitude_default));
+
+        String orderBy = prfs.getString(getString(R.string.settings_order_by_key), getString(R.string.settings_order_by_default));
+
         Uri baseUri = Uri.parse(EARTHQUAKE_BASE_URL);
         Uri.Builder uriBuilder = baseUri.buildUpon();
         uriBuilder.appendQueryParameter("format", "geojson");
         uriBuilder.appendQueryParameter("limit", "10");
         uriBuilder.appendQueryParameter("minmag", magnitude);
-        uriBuilder.appendQueryParameter("orderby", "time");
+        uriBuilder.appendQueryParameter("orderby", orderBy);
 
 
         return new EarthquakeLoader(this, uriBuilder.toString());
--- quakeReport/app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ quakeReport/app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.quakereport;
 
 import android.content.SharedPreferences;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -20,22 +21,36 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
-            Preference pref = findPreference(getString(R.string.settings_min_magnitude_key));
-            bindPreferenceSummaryToValue(pref);
+            Preference magPref = findPreference(getString(R.string.settings_min_magnitude_key));
+            String magPrefDefault = getString(R.string.settings_min_magnitude_default);
+            bindPreferenceSummaryToValue(magPref, magPrefDefault);
+
+            Preference orderByPref = findPreference(getString(R.string.settings_order_by_key));
+            String orderByDefault = getString(R.string.settings_order_by_default);
+            bindPreferenceSummaryToValue(orderByPref, orderByDefault);
         }
 
         @Override
         public boolean onPreferenceChange(Preference preference, Object o) {
             String value = o.toString();
-            preference.setSummary(value);
+            if(preference instanceof ListPreference){
+                ListPreference listPreference = (ListPreference)preference;
+                int prefIndex = listPreference.findIndexOfValue(value);
+                if(prefIndex >= 0){
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+            }else{
+                preference.setSummary(value);
+            }
             return true;
         }
 
-        private void bindPreferenceSummaryToValue(Preference magPref){
-            magPref.setOnPreferenceChangeListener(this);
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(magPref.getContext());
-            String prefString = prefs.getString(getString(R.string.settings_min_magnitude_key), getString(R.string.settings_min_magnitude_default));
-            onPreferenceChange(magPref, prefString);
+        private void bindPreferenceSummaryToValue(Preference preference, String defaultVal){
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String prefString = prefs.getString(preference.getKey(), defaultVal);
+            onPreferenceChange(preference, prefString);
         }
     }
 }
